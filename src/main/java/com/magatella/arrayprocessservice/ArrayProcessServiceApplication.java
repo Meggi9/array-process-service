ce57e88d65d43f737b632984b77995c4905c709d
Magomed Muradkhanov <magamuradkhanov@gmail.com>
2022-10-22 14:07:31 +0300

    Initial commit

---
+++ src/main/java/com/magatella/arrayprocessservice/ArrayProcessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.magatella.arrayprocessservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrayProcessServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArrayProcessServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/magatella/arrayprocessservice/ArrayProcessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magatella.arrayprocessservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArrayProcessServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
