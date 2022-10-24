5b97f784beb9e45f515464f02af38ede4d582b63
Magomed Muradkhanov <magamuradkhanov@gmail.com>
2022-10-24 16:20:55 +0300

    Add upload file in HTTP request

---
+++ src/main/java/com/magatella/arrayprocessservice/controllers/FileController.java
@@ -0,0 +1,38 @@
+package com.magatella.arrayprocessservice.controllers;
+
+import com.magatella.arrayprocessservice.models.ResponseDTO;
+import com.magatella.arrayprocessservice.services.ArrayService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@RestController
+public class FileController {
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    ArrayService arrayService = new ArrayService();
+
+    public FileController(ArrayService arrayService) {
+        this.arrayService = arrayService;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/upload")
+    public ResponseEntity<ResponseDTO> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("accept", "application/xml");
+        headers.add("Content-Transfer-Encoding", "binary");
+
+        File fullFilepath = new File(System.getProperty("user.dir") +"/upload/"+file.getOriginalFilename());
+        file.transferTo(fullFilepath);
+
+
+        return new ResponseEntity<>(new ResponseDTO((arrayService.findMaxValue(fullFilepath.getAbsolutePath()))), headers, HttpStatus.OK);
+
+    }
+}
--- src/main/java/com/magatella/arrayprocessservice/services/ArrayService.java
+++ src/main/java/com/magatella/arrayprocessservice/services/ArrayService.java
@@ -1,9 +1,14 @@
 package com.magatella.arrayprocessservice.services;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
